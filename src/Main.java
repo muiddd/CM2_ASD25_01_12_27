d8ac87f88f79bbf3088155c6c856e5d3d3577c24
muiddd <muidabdulmuid99@gmail.com>
2025-05-28 10:40:29 +0700

    p

--- src/Main.java
+++ src/Main.java
@@ -32,14 +32,13 @@ public class Main {
                     antrian.addLast(pasien);
                     break;
                 case 2:
-                    
+
                     break;
                 case 3:
                     break;
                 case 4:
                     break;
                 case 5:
-
                     break;
                 case 6:
                     break;
